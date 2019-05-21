628bf06ad2c72eb6da251926f064b11017b29b93
marisa paramita <chicha.marisa@gmail.com>
2019-05-21 18:23:04 +0100

    Creating a booking controller, fixing sign in and login page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :items, only: [:index, :create, :new] do
+  resources :items, only: [:index, :show, :create, :new] do
     resources :booking, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
 end
