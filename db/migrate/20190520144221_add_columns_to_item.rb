3d00290f242b9671ab2e65a56b70f1f250bfe7e9
Antoinette Monnier <antoinette.monnier1@gmail.com>
2019-05-21 17:44:31 +0100

    resolved pb

--- db/migrate/20190520144221_add_columns_to_item.rb
+++ db/migrate/20190520144221_add_columns_to_item.rb
@@ -1,7 +1,7 @@
 class AddColumnsToItem < ActiveRecord::Migration[5.2]
   def change
     add_column :items, :description, :text
-    add_column :items, :photo, :text
+    add_column :items, :photo, :string
     add_column :items, :price, :float
     add_column :items, :availability, :date
     add_reference :items, :user, foreign_key: true
