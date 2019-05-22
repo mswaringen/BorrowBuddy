490500ad61df21db8ca55e516efbcc7a59d8cf73
Antoinette Monnier <antoinette.monnier1@gmail.com>
2019-05-22 14:40:26 +0100

    created user profile page with listing

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,6 +30,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:title, :photo, :price, :availability)
+    params.require(:item).permit(:title, :photo, :price, :availability, :description)
   end
 end
