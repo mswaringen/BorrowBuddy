8096cb1d70d03ed0706243775d017e0eae223e62
Antoinette Monnier <antoinette.monnier1@gmail.com>
2019-05-22 16:38:22 +0100

    Added user info in profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :items, foreign_key: "owner_id", dependent: :destroy
   has_many :bookings
+  has_one :profile
+  after_save :create_profile
 
   validates :email, uniqueness: true, presence: true
+
+
+  def create_profile
+    profile = Profile.new
+    profile.user = self
+    profile.save
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_132128) do
+ActiveRecord::Schema.define(version: 2019_05_22_144025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2019_05_22_132128) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -57,4 +64,5 @@ ActiveRecord::Schema.define(version: 2019_05_22_132128) do
   add_foreign_key "bookings", "items"
   add_foreign_key "bookings", "users"
   add_foreign_key "items", "users"
+  add_foreign_key "profiles", "users"
 end
