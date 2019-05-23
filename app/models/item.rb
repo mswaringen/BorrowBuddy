af0cee4476b81a5a64ff3d3923fec344bc4b08bb
Antoinette Monnier <antoinette.monnier1@gmail.com>
2019-05-23 15:51:16 +0100

    added booking to profile

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many :bookings
+  has_many :users, through: :bookings
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :items, only: [:index, :show, :create, :new] do
-    resources :bookings, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    resources :bookings
   end
 
     get "profile", to: "profiles#dashboard"
